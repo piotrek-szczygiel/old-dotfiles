768c4d800b05dd3c5d61279ce5d988c6d4cbda60
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-06-28 00:15:43 +0200

    spacemacs: Bind <SPC>qw to kill the frame.

--- spacemacs_config.el
+++ spacemacs_config.el
@@ -341,9 +341,14 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
 
+  ;; Various settings
   (setq scroll-margin 5
         vc-follow-symlinks t
         x-select-enable-clipboard-manager nil)
+
+  ;; Keybindings
+  (spacemacs/set-leader-keys "qw" 'spacemacs/frame-killer)
+
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
