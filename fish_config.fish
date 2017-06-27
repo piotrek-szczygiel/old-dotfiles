2b9223276e61cf7667bd1442cbca095728cf82f9
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-06-28 00:01:23 +0200

    fish: change indentation from 4 spaces to 2 spaces.

--- fish_config.fish
+++ fish_config.fish
@@ -6,8 +6,8 @@ set -xg PATH ~/.local/bin $PATH
 
 # Go binaries PATH
 if type -q go
-    set -xg PATH ~/go/bin $PATH
-    set -xg GOPATH ~/go
+  set -xg PATH ~/go/bin $PATH
+  set -xg GOPATH ~/go
 end
 
 
@@ -21,103 +21,103 @@ set -xg theme_display_user yes
 ## Spacemacs settings
 # Emacs ansi-term support
 if test -n "$EMACS"
-    set -x TERM eterm-color
+  set -x TERM eterm-color
 end
 
 # Fix showing title in spacemacs shell
 function fish_title
-    true
+  true
 end
 
 
 ## Aliases
 # Alias for spacemacs daemon gui
 function smdg
-    systemctl --user $argv emacsgui
+  systemctl --user $argv emacsgui
 end
 
 # Alias for spacemacs daemon terminal
 function smdt
-    systemctl --user $argv emacsterm
+  systemctl --user $argv emacsterm
 end
 
 # Alias for spacemacs gui client
 function smg
-    emacsclient -s gui -c $argv
+  emacsclient -s gui -c $argv
 end
 
 # Alias for spacemacs terminal client
 function smt
-    emacsclient -s term -t $argv
+  emacsclient -s term -t $argv
 end
 
 # Some simple git aliases
 if type -q git
-    function ga --description 'Alias for git add'
-        git add $argv
-    end
+  function ga --description 'Alias for git add'
+    git add $argv
+  end
 
-    function gd --description 'Alias for git diff'
-        git diff $argv
-    end
+  function gd --description 'Alias for git diff'
+    git diff $argv
+  end
 
-    function gc --description 'Alias for git commit'
-        git commit -m $argv
-    end
+  function gc --description 'Alias for git commit'
+    git commit -m $argv
+  end
 
-	function gca --description 'Alias for git commit -a'
-		git commit -am $argv
-	end
+  function gca --description 'Alias for git commit -a'
+    git commit -am $argv
+  end
 
-    function gp --description 'Alias for git push'
-        git push $argv
-    end
+  function gp --description 'Alias for git push'
+    git push $argv
+  end
 
-    function gs --description 'Alias for git status'
-        git status $argv
-    end
+  function gs --description 'Alias for git status'
+    git status $argv
+  end
 end
 
 # Connect to ssh with rmate functionallity
 # Alias: rssh = ssh -R 52698:localhost:52698 user@host
 function rssh --description 'Start ssh connection with rmate forwarding'
-    ssh -R 52698:localhost:52698 $argv
+  ssh -R 52698:localhost:52698 $argv
 end
 
 # Set EDITOR depending on which editor is installed
 if type -q smt
-    set -xg EDITOR smt
+  set -xg EDITOR "emacsclient -s term -t"
 else if type -q nvim
-    set -xg EDITOR nvim
+  set -xg EDITOR nvim
 else if type -q vim
-    set -xg EDITOR vim
+  set -xg EDITOR vim
 else if type -q nano
-    set -xg EDITOR nano
+  set -xg EDITOR nano
 end
 
 
 # Set up fancy colors and icons for ls
 if type -q ls_colors_generator; and type -q ls-i
-    set -xg LS_COLORS (ls_colors_generator)
+  set -xg LS_COLORS (ls_colors_generator)
 
-    function ls --description 'List files with fancy devicons'
-        ls-i --color=auto -w (tput cols) $argv
-    end
+  function ls --description 'List files with fancy devicons'
+    ls-i --color=auto -w (tput cols) $argv
+  end
 
-    function la --description 'List files with fancy devicons'
-        ls -la --group-directories-first $argv
-    end
+  function la --description 'List files with fancy devicons'
+    ls -la --group-directories-first $argv
+  end
 
-    function ll --description 'List files with fancy devicons'
-        ls --almost-all -1 --group-directories-first $argv
-    end
+  function ll --description 'List files with fancy devicons'
+    ls --almost-all -1 --group-directories-first $argv
+  end
 
-    function l --description 'List files with fancy devicons'
-        ls -a --group-directories-first $argv
-    end
+  function l --description 'List files with fancy devicons'
+    ls -a --group-directories-first $argv
+  end
 end
 
 # virtualenv (Python3 virtualfish)
 if python3 -m virtualfish > /dev/null ^&1
-    eval (python3 -m virtualfish auto_activation) or true
+  eval (python3 -m virtualfish auto_activation) or true
 end
