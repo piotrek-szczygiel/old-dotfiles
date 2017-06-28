12c6369a34d2cfe66b53b408169a376babb7db93
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-06-28 02:21:17 +0200

    spacemacs: Use eshell as default shell and sort python imports on save

--- spacemacs_config.el
+++ spacemacs_config.el
@@ -1,6 +1,4 @@
 ;; -*- mode: emacs-lisp -*-
-;; This file is loaded by Spacemacs at startup.
-;; It must be stored in your home directory.
 
 (defun dotspacemacs/layers ()
   "Configuration Layers declaration.
@@ -40,7 +38,8 @@ values."
      html
      javascript
      markdown
-     python
+     (python :variables
+             python-sort-imports-on-save t)
      shell-scripts
      vimscript
      yaml
@@ -49,7 +48,8 @@ values."
      github
      version-control
 
-     shell
+     (shell :variables
+            shell-default-shell 'eshell)
      systemd
 
      spotify
@@ -305,13 +305,7 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
-
-  (setq scroll-margin 5
-        vc-follow-symlinks t)
-
-  (spacemacs/set-leader-keys "qw" 'spacemacs/frame-killer)
-
-  )
+)
 
 (defun dotspacemacs/user-config ()
   "Configuration function for user code.
@@ -320,7 +314,11 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
-  )
+
+  (setq scroll-margin 5
+        vc-follow-symlinks t)
+
+  (spacemacs/set-leader-keys "qw" 'spacemacs/frame-killer))
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
