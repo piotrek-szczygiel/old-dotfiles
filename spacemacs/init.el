9d6a324d0f7e6a78a690178db1f1ef782b9b3dd6
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-07-11 23:11:02 +0200

    Use semi-bold font

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -153,9 +153,9 @@ It should only modify the values of Spacemacs settings."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("SauceCodePro Nerd Font 16"
-                               :size 16
-                               :weight normal
+   dotspacemacs-default-font '("SauceCodePro Nerd Font"
+                               :size 18
+                               :weight semi-bold
                                :width normal
                                :powerline-scale 1.1)
    ;; The leader key
