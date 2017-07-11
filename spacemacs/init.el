9fd32759c22023885fdd4c212da31251daf66c6b
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-07-11 19:21:44 +0200

    Add 'fnm' bin directory to emacs bin path

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -355,6 +355,7 @@ This function is called immediately after `dotspacemacs/init', before layer
 configuration.
 It is mostly for variables that should be set before packages are loaded.
 If you are unsure, try setting them in `dotspacemacs/user-config' first."
+  (add-to-list 'exec-path "~/.config/fnm/bin" t)
   )
 
 (defun dotspacemacs/user-config ()
