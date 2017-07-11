250c35f94974a1d94acbbc44d34e6d0c80ccf3ef
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-07-11 23:10:48 +0200

    Use dracula colorscheme

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -30,7 +30,6 @@ This function should only modify configuration layer settings."
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
-
      ;; Checkers
      syntax-checking
 
@@ -53,6 +52,7 @@ This function should only modify configuration layer settings."
      javascript
      (markdown :variables
                markdown-live-preview-engine 'vmd)
+     markdown
      python
      shell-scripts
      typescript
@@ -146,7 +146,8 @@ It should only modify the values of Spacemacs settings."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-dark
+   dotspacemacs-themes '(dracula
+                         spacemacs-dark
                          spacemacs-light)
    ;; If non-nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
