a7373ece28742e854673e265bfa1fd274ca09a40
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-07-13 18:26:05 +0200

    Use gotham theme

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -148,8 +148,7 @@ It should only modify the values of Spacemacs settings."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(dracula
-                         spacemacs-dark
+   dotspacemacs-themes '(gotham
                          spacemacs-light)
    ;; If non-nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
