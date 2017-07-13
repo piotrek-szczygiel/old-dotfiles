e4af51bfcc8a62036d07524bffe0ce600f8cee63
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-07-13 18:24:45 +0200

    Fix fzf-ing into spacemacs, use 2 space indentation

--- fish/config.fish
+++ fish/config.fish
@@ -7,7 +7,7 @@ set -xg PATH $PATH $GOPATH/bin
 alias e="$EDITOR"
 alias o="xdg-open"
 alias q="exit"
-alias s="emacsclient -n -c"
+alias s="spacemacs"
 
 # Git aliases
 alias ga="git add"
@@ -22,27 +22,37 @@ alias gs="git status"
 set -xg FZF_DEFAULT_COMMAND 'rg --files --hidden --smart-case --glob "!.git/*"'
 set -xg FZF_FIND_FILE_COMMAND $FZF_DEFAULT_COMMAND
 
+function spacemacs
+  emacsclient -n -c $argv
+end
+
+function __fzf_spacemacs
+  set -l filename (fzf)
+  if [ $filename ]
+    spacemacs $filename
+  end
+end
+
 # Keybindings
 function fish_user_key_bindings
-    bind \cd "__fzf_cd_with_hidden"
-    bind \cn "nvim (fzf)"
-    bind \cr "__fzf_reverse_isearch"
-    bind \cs "s (fzf)"
-    bind \ct "__fzf_find_file"
+  bind \cd "__fzf_cd_with_hidden"
+  bind \cr "__fzf_reverse_isearch"
+  bind \cs "__fzf_spacemacs"
+  bind \ct "__fzf_find_file"
 end
 
 # Colored man pages
 function man
-    which man | read MAN
+  which man | read MAN
 
-    set -x LESS_TERMCAP_md (printf "\e[01;31m")
-    set -x LESS_TERMCAP_me (printf "\e[0m")
-    set -x LESS_TERMCAP_se (printf "\e[0m")
-    set -x LESS_TERMCAP_so (printf "\e[01;44;33m")
-    set -x LESS_TERMCAP_ue (printf "\e[0m")
-    set -x LESS_TERMCAP_us (printf "\e[01;32m")
+  set -x LESS_TERMCAP_md (printf "\e[01;31m")
+  set -x LESS_TERMCAP_me (printf "\e[0m")
+  set -x LESS_TERMCAP_se (printf "\e[0m")
+  set -x LESS_TERMCAP_so (printf "\e[01;44;33m")
+  set -x LESS_TERMCAP_ue (printf "\e[0m")
+  set -x LESS_TERMCAP_us (printf "\e[01;32m")
 
-    eval $MAN $argv
+  eval $MAN $argv
 end
 
 # Emacs ansi-term support
@@ -56,7 +66,7 @@ end
 
 set -xg LS_COLORS (ls_colors_generator)
 function ls
-    ls-i --color=always -A --group-directories-first -h -w (tput cols) $argv
+  ls-i --color=always -A --group-directories-first -h -w (tput cols) $argv
 end
 
 # Virtualfish initialization
