69df467e799d8f135320e83b9ff2c3dd7b0fd8cf
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-07-17 16:40:05 +0200

    Use gotham theme

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -70,6 +70,9 @@ This function should only modify configuration layer settings."
      ;; Source control
      git
      version-control
+
+     ;; Themes
+     themes-megapack
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
@@ -146,8 +149,9 @@ It should only modify the values of Spacemacs settings."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-dark
-                         spacemacs-light)
+   dotspacemacs-themes '(gotham
+                         spacemacs-light
+                         spacemacs-dark)
    ;; If non-nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
