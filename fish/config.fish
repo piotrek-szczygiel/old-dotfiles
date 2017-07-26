07c88dc06193b4717114fbebb57bbc558bfb0150
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-07-26 12:58:38 +0200

    Add some useful git aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,10 @@
 set -xg EDITOR nvim
 set -xg PATH $HOME/.dotfiles/bin $PATH
+
+alias ga "git add"
+alias gc "git commit"
+alias gca "git commit -a"
+alias gd "git diff"
+alias gl "git pull"
+alias gp "git push"
+alias gs "git status"
