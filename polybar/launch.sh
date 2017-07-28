33f9a1546f6486cd035a52381d9d5ec0ef070502
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-07-29 00:51:16 +0200

    Add net, updates and weather modules

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,8 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-#polybar -r bar1 &
 polybar -r top &
 polybar -r bottom &
 
