32059fb6e8f2e65a6a798e2dc754b48735c8a2fc
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-10-15 21:42:43 +0200

    Use wp.pl instead of stackoverflow.com

--- polybar/ping.sh
+++ polybar/ping.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PING=$(ping -c 4 www.stackoverflow.com | tail -1| awk '{print $4}' |\
+PING=$(ping -c 4 www.wp.pl 2> /dev/null | tail -1| awk '{print $4}' |\
     cut -d '/' -f 2 | cut -f1 -d".")
 
 echo -n "%{F#bd93f9}%{F-} ${PING}ms"
