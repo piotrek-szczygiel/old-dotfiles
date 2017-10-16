37447b3fb51152cf2b0ef89219c83af003d43231
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-10-16 10:43:24 +0200

    Ping every second, change color on error

--- polybar/ping.sh
+++ polybar/ping.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-PING=$(ping -c 4 www.wp.pl 2> /dev/null | tail -1| awk '{print $4}' |\
+PING=$(ping -c 1 www.wp.pl 2> /dev/null | tail -1| awk '{print $4}' |\
     cut -d '/' -f 2 | cut -f1 -d".")
 
 if [ ! -z "${PING}" ]; then
     echo -n "%{F#bd93f9}%{F-} ${PING}ms"
+else
+    echo -n "%{F#ef9329}%{F-}"
 fi
