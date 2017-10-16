36740740dc522caaf19392eb26a3423d7b448339
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-10-16 21:20:40 +0200

    Add volume module

---
+++ polybar/redshift.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+icon=""
+
+pgrep -x redshift &> /dev/null
+if [[ $? -eq 0 ]]; then
+    temp=$(redshift -p 2>/dev/null | grep temp | cut -d' ' -f3)
+    temp=${temp//K/}
+fi
+
+# OPTIONAL: Append ' ${temp}K' after $icon
+if [[ -z $temp ]]; then
+    echo "%{F#65737E}$icon"       # Greyed out (not running)
+elif [[ $temp -ge 5000 ]]; then
+    echo "%{F#8FA1B3}$icon"       # Blue
+elif [[ $temp -ge 4000 ]]; then
+    echo "%{F#EBCB8B}$icon"       # Yellow
+else
+    echo "%{F#D08770}$icon"       # Orange
+fi
