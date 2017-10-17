c09f7240c429a5c9a7e7ea4c136afa8bd4d0acaa
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-10-17 17:00:33 +0200

    Hide updates module when update has finished

--- polybar/updates.sh
+++ polybar/updates.sh
@@ -6,5 +6,7 @@ aur=$(cower -u 2> /dev/null | wc -l)
 check=$((pac + aur))
 
 if [[ "$check" != "0" ]]; then
-    echo "$pac %{F#5b5b5b}%{F-} $aur"
+    echo "%{F#8be9fd}%{F-} $pac %{F#5b5b5b}%{F-} $aur"
+else
+    echo " "
 fi
