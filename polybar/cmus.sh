bc9ece6fa8a578de9d6e7ebb9218213d68b15f93
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-10-17 17:23:17 +0200

    Fix weird song name when nothing is playing

--- polybar/cmus.sh
+++ polybar/cmus.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-artist=$(echo -n $(cmus-remote -C status | grep artist -m 1 | cut -c 12-))
-song=$(echo -n $(cmus-remote -C status | grep title -m 1 | cut -c 11-))
+artist=$(echo -n $(cmus-remote -C status | grep "tag artist" -m 1 | cut -c 12-))
+song=$(echo -n $(cmus-remote -C status | grep "tag title" -m 1 | cut -c 11-))
 
-echo -n "$artist - $song"
+if [ ! -z "$artist" ]; then
+    echo -n "%{F#bd93f9}%{F-} $artist - $song"
+else
+    echo -n " "
+fi
