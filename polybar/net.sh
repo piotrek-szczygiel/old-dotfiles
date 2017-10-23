543af3419a58b988361b183504c9a9d8cae6e487
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-10-23 18:50:08 +0200

    Fetch external IP from different URL

--- polybar/net.sh
+++ polybar/net.sh
@@ -3,14 +3,15 @@
 LOCAL_IP=$(timeout 1 ip route get 8.8.8.8 2> /dev/null |\
     head -1 | cut -d' ' -f7)
 
-EXTERNAL_IP=$(timeout 5 curl icanhazip.com 2> /dev/null)
+EXTERNAL_IP=$(timeout 5 curl https://canihazip.com/s 2> /dev/null)
 
 if [ "$LOCAL_IP" == "192.168.2.27" ] || \
    [ "$LOCAL_IP" == "192.168.8.100" ]; then
     echo -n "%{F#8bbcd2}%{F-} LTE"
 elif [ "$LOCAL_IP" == "192.168.2.28" ]; then
     echo -n "%{F#e89a00}%{F-} ADSL"
-elif [ "$EXTERNAL_IP" == "149.156.124.14" ]; then
+elif [ "$EXTERNAL_IP" == "149.156.124.14" ] || \
+     [ "$EXTERNAL_IP" == "149.156.126.10" ]; then
     echo -n "%{F#13eb34}%{F-} AGH"
 elif [ ! -z "$EXTERNAL_IP" ]; then
     echo -n "%{F#a0d18a}%{F-} $EXTERNAL_IP"
