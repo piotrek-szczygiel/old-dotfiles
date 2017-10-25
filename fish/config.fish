d2c930866acbca731e79511d1cabfb172e93208b
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-10-25 12:31:30 +0200

    Don't show fzf errors

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 set -xg EDITOR nvim
 set -xg PATH $HOME/.dotfiles/bin $HOME/.local/bin $PATH
-set -xg FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+set -xg FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*" 2>/dev/null'
 set -xg FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 
 set -g theme_title_display_process yes
