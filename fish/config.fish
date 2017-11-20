1d101430cd9bd7ae4cbbeb716c2ad4bb5297661a
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-11-20 21:14:48 +0100

    Change aliases, add default editor for ranger

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,10 @@
 set -xg EDITOR nvim
+set -xg VISUAL $EDITOR
 set -xg PATH $HOME/.dotfiles/bin $HOME/.local/bin $PATH
 set -xg FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*" 2>/dev/null'
 set -xg FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 
 set -g theme_title_display_process yes
-
-alias ls "better_ls"
 
 alias ga "git add"
 alias gc "git commit"
@@ -18,26 +17,23 @@ alias gp "git push"
 alias grh "git reset HEAD"
 alias gs "git status"
 
+alias ls "better_ls"
+
 alias wifi "nmtui-connect"
 
 alias o "xdg-open"
 
+alias plan "feh ~/Dropbox/plan.png"
+
 alias q "exit"
 
-alias td "nvim ~/.todo"
+alias td "$EDITOR ~/.todo"
 
 alias update "pacaur -Syu"
 
+alias vi "$EDITOR"
+alias vim "$EDITOR"
+alias svi "sudo -E $EDITOR"
+
 alias yt "youtube-viewer"
 
-function plan
-    feh ~/MEGA/plan.png
-end
-
-function vi
-    nvim $argv
-end
-
-function svi
-    sudo -E nvim $argv
-end
