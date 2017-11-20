fe8569cfb781bd277922ab11c3c76ce6c4a540df
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-11-20 15:16:14 +0100

    Change network module icons

--- polybar/net.sh
+++ polybar/net.sh
@@ -7,20 +7,17 @@ LOCAL_IP=$(timeout 1 ip route get 8.8.8.8 2> /dev/null |\
 
 EXTERNAL_IP=$(timeout 5 curl https://canihazip.com/s 2> /dev/null)
 
-if [ "$LOCAL_IP" == "192.168.2.27" ] || \
-   [ "$LOCAL_IP" == "192.168.8.100" ]; then
-    echo -n "%{F${COLOR}}%{F-} LTE"
-elif [ "$LOCAL_IP" == "192.168.2.28" ]; then
-    echo -n "%{F${COLOR}}%{F-} ADSL"
+if [ "$LOCAL_IP" == "192.168.8.100" ]; then
+    echo -n "%{F${COLOR}}%{F-} LTE"
 elif [ "$EXTERNAL_IP" == "149.156.124.14" ] || \
      [ "$EXTERNAL_IP" == "149.156.126.10" ]; then
-    echo -n "%{F${COLOR}}%{F-} AGH"
+    echo -n "%{F${COLOR}}%{F-} AGH"
 elif [ ! -z "$EXTERNAL_IP" ]; then
-    echo -n "%{F${COLOR}}%{F-} $EXTERNAL_IP"
+    echo -n "%{F${COLOR}}%{F-} $EXTERNAL_IP"
 elif [ ! -z "$LOCAL_IP" ]; then
-    echo -n "%{F${COLOR}}%{F-} $LOCAL_IP"
+    echo -n "%{F${COLOR}}%{F-} $LOCAL_IP"
 else
-    echo -n "%{F${COLOR}}%{F-} Unknown IP"
+    echo -n "%{F${COLOR}}%{F-} Unknown IP"
 fi
 
 PING=$(timeout 5 ping -c 1 www.wp.pl 2> /dev/null |\
