8bc3695e04d1d006f768b695bcbf2318c6f3a2e7
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-11-21 22:59:27 +0100

    Fix gaps and layout binding. Use neofetch in terminal workspace

--- i3/terminal.sh
+++ i3/terminal.sh
@@ -5,7 +5,7 @@ status=$?
 i3-msg "workspace 0:terminal"
 
 if [ $status -ne 0 ]; then
-    st &
+    st -e fish -c "neofetch; and fish" &
 fi
 
 i3-msg -t get_workspaces | grep "0:terminal\",\"visible\":true"
