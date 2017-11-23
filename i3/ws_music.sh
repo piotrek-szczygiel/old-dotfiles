5e48242616cf0f3f055750f77c678ababc816b35
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-11-23 22:19:56 +0100

    Change music workspace a little bit

--- i3/ws_music.sh
+++ i3/ws_music.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
 
-i3-msg -t get_workspaces | grep "102:music"
+i3-msg -t get_workspaces | grep "101:music"
 status=$?
-i3-msg "workspace 102:music"
+i3-msg "workspace 101:music"
 
 if [ $status -ne 0 ]; then
     ~/.dotfiles/i3/term.sh ncmpcpp &
-    sleep 0.1s
+    sleep 0.2s
 
     ~/.dotfiles/i3/term.sh cava &
-    sleep 0.1s
-
-    i3-msg "split vertical"
-    ~/.dotfiles/i3/term.sh ncmpcpp -s clock &
-    sleep 0.1s
+    sleep 0.2s
 
     i3-msg "focus left"
 fi
 
-i3-msg -t get_workspaces | grep "102:music\",\"visible\":true"
+i3-msg -t get_workspaces | grep "101:music\",\"visible\":true"
 status=$?
 
 if [ $status -ne 1 ]; then
