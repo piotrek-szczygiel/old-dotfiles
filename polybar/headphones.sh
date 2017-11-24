287c60a62a47d8d73fbf9d2a16af963d6c5d6ecc
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-11-24 16:55:31 +0100

    Use new headphones

--- polybar/headphones.sh
+++ polybar/headphones.sh
@@ -3,7 +3,7 @@
 COLOR=$(xrdb -query | grep color15 | cut -f2 | head -1 2> /dev/null)
 INACTIVE=$(xrdb -query | grep color8 | cut -f2 | head -1 2> /dev/null)
 
-echo -e "quit" | bluetoothctl | grep -q "\[Philips SHB4000\]"
+echo -e "quit" | bluetoothctl | grep -q "\[DENON AH-GC20\]"
 
 if [ $? -eq 0 ]; then
     echo -n "%{F${COLOR}}"
