24909c8e6925ad60e3ca0cf4ff492606fd4e6ed0
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-12-14 19:12:21 +0100

    Remove unused configurations

--- i3/wal.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-
-index=$(basename $(cat ~/.fehbg | tail -1 | cut -d" " -f3 | cut -d\' -f2) .jpg)
-count=$(ls -l ~/Dropbox/wallpapers | tail -n +2 | wc -l)
-
-index=$(($index + 1))
-index=$(($index % $count))
-
-wal -a 90 -i ~/Dropbox/wallpapers/$index.jpg
