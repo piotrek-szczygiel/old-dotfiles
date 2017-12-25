2d032dc2d31b8e9e73668dc1c5d5faf79bc052ce
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-12-26 00:52:49 +0100

    Add directories to PATH only if they exist

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,13 @@ set -xg VISUAL $EDITOR
 
 set -xg MANPAGER "nvim -c 'set ft=man' -"
 
-set -xg PATH $HOME/.dotfiles/bin $HOME/.cargo/bin $HOME/.local/bin $PATH
+set -xg PATH $HOME/.dotfiles/bin $PATH
+if test -d "$HOME/.cargo/bin"
+  set -xg PATH $HOME/.cargo/bin $PATH
+end
+if test -d "$HOME/.local/bin"
+  set -xg PATH $HOME/.local/bin $PATH
+end
 
 alias aruba "ssh -t piotr@szczygiel.tk ~/arubasession"
 alias l "ls -l"
@@ -33,9 +39,9 @@ alias grh "git reset HEAD"
 alias gs "git status"
 
 function q
-    if tmux display-message -p '#S' ^ /dev/null | grep -q "aruba"
-        tmux detach ^ /dev/null
-    else
-        exit
-    end
+  if tmux display-message -p '#S' ^ /dev/null | grep -q "aruba"
+    tmux detach ^ /dev/null
+  else
+    exit
+  end
 end
