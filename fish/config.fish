fbddcb51c6bde26b59c2dedfa3ce0e5f3053e245
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-12-27 11:19:10 +0100

    Add fish plugins: fasd, !!, pure theme

--- fish/config.fish
+++ fish/config.fish
@@ -21,9 +21,11 @@ alias lst "ls --tree"
 alias o "xdg-open"
 alias orphans "pacaur -Rns (pacaur -Qtdq)"
 alias plan "xdg-open ~/Dropbox/plan.png > /dev/null ^&1"
+alias q "exit"
 alias root "sudo -E bash"
 alias td "$EDITOR ~/.todo"
 alias update "pacaur -Syu"
+alias v "f -e $EDITOR"
 alias vi "$EDITOR"
 
 alias ga "git add"
@@ -37,13 +39,5 @@ alias glg "git lg"
 alias gp "git push"
 alias grh "git reset HEAD"
 alias gs "git status"
-
-function q
-  if tmux display-message -p '#S' ^ /dev/null | grep -q "aruba"
-    tmux detach ^ /dev/null
-  else
-    exit
-  end
-end
 
 sh ~/.dotfiles/tmux/gruvbox.sh
