b8ccb686fdb457dc6368eda75ed4a524b94743b6
Piotr Szczygieł <szczygiel.p@gmail.com>
2017-12-28 13:37:34 +0100

    Configure tmux status bar (battery, username module)

---
+++ tmux/username.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+tty=${1:-$(tmux display -p '#{pane_tty}')}
+username=$(ps -t "$tty" -o user= -o pid= -o ppid= -o command= | awk '
+  !/ssh/ { user[$2] = $1; ppid[$3] = 1 }
+  END {
+    for (i in user)
+      if (!(i in ppid))
+      {
+        print user[i]
+        exit
+      }
+  }
+')
+
+hostname=$(hostname)
+
+printf "#[fg=#1d2021,bg=white,bold] "
+
+if [ "$username" = "root" ]; then
+  printf "#[fg=colour1]root!"
+else
+  printf "$username"
+fi
+
+printf "#[fg=colour0,nobold]"
+printf "@$hostname "
